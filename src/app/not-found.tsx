031eaebee3387c131bdb945abb06c18d1b073442
Max Hajek <maxhajek@outlook.de>
2024-01-16 00:10:06 +0100

    update layout component for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,11 @@
-"use client";
-
-import Error from "next/error";
-
-// Render the default Next.js 404 page when a route
-// is requested that doesn't match the middleware and
-// therefore doesn't have a locale associated with it.
+import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <html lang="en">
-      <body>
-        <Error statusCode={404} />
-      </body>
-    </html>
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
   );
 }
